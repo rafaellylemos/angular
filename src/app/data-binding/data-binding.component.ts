44a0df25a4221cf904ce2ad701826bb1a32df42f
rafaellylemos <rafaellylemos@hotmail.com>
2024-01-04 10:33:28 -0300

    first commit

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputPropertyComponent } from '../input-property/input-property.component';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [FormsModule, InputPropertyComponent],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+  nome: string = 'abc';
+
+  pessoa: any = {
+    nome: 'Rafa',
+    idade: 34,
+    end: {
+      cidade: 'Recife',
+      estado: 'PE'
+    }
+  }
+
+  nomeDoCurso: string = 'Angular';
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(event: any) {
+    console.log(event.target.value)
+    this.valorSalvo = event.target.value;
+  }
+
+  OnMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  novoNome(event: Event) {
+    console.log((event.target as HTMLInputElement).value);
+    this.nome = (event.target as HTMLInputElement).value;
+  }
+
+  //salvarValor e novoNome são duas formas diferentes de chegar ao mesmo resultado. Pode-se usar qualquer uma. Mas é peferível sempre optar pela verificação de tipo precisa (Ex.: event: Event) para evitar possíveis erros e tornar o código mais confiável.
+
+}
---
+++ src/app/input-property/input-property.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-property',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-property.component.html',
+  styleUrl: './input-property.component.css'
+})
+export class InputPropertyComponent {
+  @Input() nome: string = '';
+}
