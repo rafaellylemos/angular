daf601f2aba1416c455abb1ca11244dad06e9707
rafaellylemos <rafaellylemos@hotmail.com>
2024-01-04 14:42:37 -0300

    Lifecycles hooks (att Angular17)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DataBindingComponent],
+  imports: [CommonModule, RouterOutlet, DataBindingComponent, CicloComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso';
+  valor: number = 5;
+
+  deletarCiclo: boolean = false;
+
+  mudarValor() {
+    this.valor++;
+  }
+
+  destruirCiclo() {
+    this.deletarCiclo = true;
+  }
 }
