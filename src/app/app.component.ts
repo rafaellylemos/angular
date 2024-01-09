5d27c5f7220ca7be02114ff3072f7fc5788ca206
rafaellylemos <rafaellylemos@hotmail.com>
2024-01-09 12:00:45 -0300

    bootstrap test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,18 @@ import { CommonModule, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { CicloComponent } from './ciclo/ciclo.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DataBindingComponent, CicloComponent, NgIf],
+  imports: [CommonModule, RouterOutlet, DataBindingComponent, CicloComponent, NgIf, MenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title: string = 'curso';
+
   valor: number = 5;
 
   deletarCiclo: boolean = false;
